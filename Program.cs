50e20b5ec2cd90ca41c0c350ffcddcebb84be895
hjeyhun <158857537+hjeyhun@users.noreply.github.com>
2024-02-11 15:36:42 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c, sonuc;
+
+            a = 5;
+            b = 4;
+            c = 6;
+            sonuc = a + b + c;
+
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+
+        }
+    }
+}
